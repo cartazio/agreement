2578e3b045bd4154eec457dd9fd07f42e88f32e4
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2017-11-14 14:35:49 -0500

    notes on the "microservice" factorization of raft

--- src/Control/Agreement/Leader/Raft/Internal.hs
+++ src/Control/Agreement/Leader/Raft/Internal.hs
@@ -5,3 +5,24 @@
 module Control.Agreement.Leader.Raft.Internal where
 
 
+{-
+Term Selection/ Stale Term
+
+Catchup
+
+Timeout / Failure Detection
+
+Leader election
+
+Append Entries Cycle
+    Is It written to disk
+    Is it Marked as Committed / accepted?
+
+Is Quorum Good ?? ??? / Membership
+
+Log GC --- Raft doesn't do GC/Snapshot in vanilla spec/ log compactions
+
+Restarts from LOCAL Log / Snapshot
+
+Disk Writes to durable log
+-}
