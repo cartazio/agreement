f70259b097fa08034577edab899b4979cb17673f
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2017-11-14 15:26:13 -0500

    more notes on the mostly  independent subsystems in raft and what they do

--- src/Control/Agreement/Leader/Raft/Internal.hs
+++ src/Control/Agreement/Leader/Raft/Internal.hs
@@ -6,19 +6,38 @@ module Control.Agreement.Leader.Raft.Internal where
 
 
 {-
-Term Selection/ Stale Term
+Term Selection/ Stale Term Detection
 
 Catchup
+  gets stuck if current leader fails!?
+  Stuck in Stale read only mode until hears from new leader?
 
-Timeout / Failure Detection
+Timeout / Failure Detection (of Current Leader of Current Term)
+  Raft approach requires both accurate timeout being some number >> networking round trip time
+  for cluster, plus randomized delay on top for symmetry breaking of candidacy
 
-Leader election
+Leader election As Candidate
+  Triggered by Failure Detector, vote for self, ask everyone for a vote
+  if enough votes, leader, tell everyone that
+    (vanilla version has no signatures to check claim etc )
+
+Leader election as Not Candidate, aka VOTER
+  Participates in those with valid term number
+  (which higher terms are valid)
 
 Append Entries Cycle
-    Is It written to disk
-    Is it Marked as Committed / accepted?
+    Persist proposal to disk on leader
+    Send AppendEntries Request from Leader
+    Is It written to disk / persisted by follower?
+    then send Append Entries Reply from Followers
+    Is it Marked as Committed / accepted by a quorum of nodes?
+    then send Advance COmmit to followers from leader
 
-Is Quorum Good ?? ??? / Membership
+Is Quorum Good ?? ???
+
+Membership
+  should only be changed by admin command
+    some intersection / time delay / propagation tricks
 
 Log GC --- Raft doesn't do GC/Snapshot in vanilla spec/ log compactions
 
