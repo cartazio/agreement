bfe9efa187e040cf36ac574eb6746fcb9dacf1f8
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2019-01-28 16:32:48 -0500

    fleshing out core protocol more

--- src/Control/Agreement/Leader/ViewStamped/FormalViewStamped.hs
+++ src/Control/Agreement/Leader/ViewStamped/FormalViewStamped.hs
@@ -12,13 +12,43 @@ notes:
 requestNumber is a per client sequencing number?
 * the execution of m/op is sometimes called an "upCall" into
 * using _ _ _ ... style notation for "bunch of parameters ill write down later"
+* OpNumber === CommitNumber == opCommitNumber, just different times
+
+coarsely speaking: 3 sub protocols
+  normal case
+  view change
+  recovery from failure  / rejoin / catchup
+
+  (also membership change, and machine local state management)
 
 record Replica  clientid operation clientRequest
 
   ClientRequestToPrimary :: operation -> clientId -> requestNumber
             -> m (Replica _ _ _ _ _ _)
-  PrimaryPrepareToReplica :: viewNumber -> operation ->  viewNumber ->
-      commitNumber -> m (Replica _ _ _ _ )
-  PrepareOkToPrimary :: viewNumber -> opNumber ->
-        replicaID -> m (Replica _ _ _ )
+    -- the included opCommitNumber is for the most recent previous
+  PrimaryPrepareToReplica ::
+    viewNumber -> operation ->  viewNumber -> opNum  -> {- last commited -} opNum ->
+         m (Replica _ _ _ _ )
+  PrepareOkToPrimary :: viewNumber -> {- proposed operation-} opNum ->
+          {-whoami-} replicaID ->
+          m (Replica _ _ _ )
+  -- add client activity idling timeout which then triggers commit
+  ClientsIdlingTimeout ::  m (Rep)
+  PrimaryCommitToReplica :: viewNumber -> opNum ->
+      m (Replica _ _ _ )
+
+  -- add Need viewchange event / decider
+  StartViewChange ::{- New view number -} viewNumber  -> replicaID ->
+      m (Replica _ _ _ )
+  DoViewChange :: {- new -} viewNumber -> logJournalRep ->
+        {- last known local normal -} viewNumber  ->  {- highest in log-} opNum  ->
+         {- highest committed -} opNum -> replicaID -> m (Replica _ _ _ _)
+  StartView :: viewNumber -> {- log from max view-} logJournalRep  ->
+    {- max in log-} opNumber ->
+    {- max commit from doview -} opNumber ->
+    m (Replica ____)
+
+
+
+
 -}
